852e5e90567d3c9faa35e62093c3a4737be4122b
won-hyw <w2019@e-mirim.hs.kr>
2021-07-27 13:38:48 +0900

    Edit MainActivity.java(Spinner Event Programming) Commit

--- app/src/main/java/kr/hs/emirim/w2019/ex11_16_17/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/ex11_16_17/MainActivity.java
@@ -3,12 +3,39 @@ package kr.hs.emirim.w2019.ex11_16_17;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    String[] itemArr = {"그랑블루", "라라랜드", "부산행", "걸캅스", "1987", "업", "토이스토리4", "어벤져스:엔드게임", "장난스런 키스", "신과함께"};
+    int[] imgIds = {R.drawable.poster01, R.drawable.poster02, R.drawable.poster03, R.drawable.poster04, R.drawable.poster05,
+                    R.drawable.poster06, R.drawable.poster07, R.drawable.poster08, R.drawable.poster09, R.drawable.poster10};
+    ImageView imgv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("스피너와 영화포스터");
+
+        imgv = findViewById(R.id.imgv);
+        Spinner spinner1 = findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, itemArr);
+        spinner1.setAdapter(adapter);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                imgv.setImageResource(imgIds[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
